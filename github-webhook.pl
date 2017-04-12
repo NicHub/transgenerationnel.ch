a82e209875009d847b62b98972410af6c292d838
NicHub <nicolas.jeanmonod@gmail.com>
2017-04-12 13:35:43 +0200

    fausse modif

--- github-webhook.pl
+++ github-webhook.pl
@@ -5,13 +5,10 @@ use warnings;
 use POSIX       qw( strftime );
 use Git;
 
-BEGIN {
-	print "Content-type: text/plain\n\n";
-}
+print "Content-type: text/plain\n\n";
 
 # Enregistre l’heure de la dernière synchronisation.
 my $now = strftime( '%Y-%m-%d %H:%M:%S', localtime );
-print "$now\n";
 my $filename = 'time.txt';
 open( my $fh, '>', $filename ) or die "Could not open file '$filename' $!";
 close $fh;
@@ -20,5 +17,3 @@ close $fh;
 my $pwd = `pwd`; chomp( $pwd );
 my $repo = Git->repository( Directory => $pwd );
 my $msg = $repo->command( "pull" );
-
-print "END"
