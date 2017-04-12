172f2de17ccac029920299115655dbc7f8bfd810
NicHub <nicolas.jeanmonod@gmail.com>
2017-04-12 13:36:56 +0200

    fausse modif

--- github-webhook.pl
+++ github-webhook.pl
@@ -7,13 +7,13 @@ use Git;
 
 print "Content-type: text/plain\n\n";
 
-# Enregistre l’heure de la dernière synchronisation.
+# Enregistre l’heure de la dernière synchronisation dans un fichier texte.
 my $now = strftime( '%Y-%m-%d %H:%M:%S', localtime );
 my $filename = 'time.txt';
 open( my $fh, '>', $filename ) or die "Could not open file '$filename' $!";
 close $fh;
 
-# Synchronisation.
+# Synchronisation du dépôt Git.
 my $pwd = `pwd`; chomp( $pwd );
 my $repo = Git->repository( Directory => $pwd );
 my $msg = $repo->command( "pull" );
