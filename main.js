09bb46e7a86396de10fd2d52e0fea61fcf81e94c
NicHub <nicolas.jeanmonod@gmail.com>
2017-04-14 14:59:25 +0200

    ajout date JSON

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+$( document ).ready( function() {
+
+
+
+  // Lit l’heure de la dernière mise à jour des fichiers
+  // (pull de Github avec Webhook)
+  ( function() {
+    var last_update = $( 'p#last-update' );
+    // On ne fait la requête que si p#last-update existe sur la page
+    if( last_update.length > 0 )
+    {
+      $.getJSON( "http://osonsnousreveler.com/github-webhook-log.json", function( data ) {
+        last_update.html( "Dernière mise à jour<br/>" + data.github_webhook_last_pull_time );
+      });
+    }
+  })();
+
+
+
+});
