c35f5d8051dd6e1136c2416f12c728860564e183
NicHub <nicolas.jeanmonod@gmail.com>
2017-04-14 14:53:21 +0200

    github-webhook.pl ⇒ JSON

--- github-webhook.pl
+++ github-webhook.pl
@@ -8,9 +8,10 @@ use Git;
 print "Content-type: text/plain\n\n";
 
 # Enregistre l’heure de la dernière synchronisation dans un fichier texte.
-my $now = strftime( '%Y-%m-%d %H:%M:%S', localtime );
-my $filename = 'time.txt';
+my $now = strftime( '{"github_webhook_last_pull_time":"%Y-%m-%d %H:%M:%S"}', localtime );
+my $filename = 'github-webhook-log.json';
 open( my $fh, '>', $filename ) or die "Could not open file '$filename' $!";
+print $fh $now;
 close $fh;
 
 # Synchronisation du dépôt Git.
