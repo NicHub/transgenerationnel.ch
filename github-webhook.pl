80df2de648216c324812a53e24970314654f0b20
NicHub <nicolas.jeanmonod@gmail.com>
2017-04-14 15:30:35 +0200

    POST

--- github-webhook.pl
+++ github-webhook.pl
@@ -7,6 +7,19 @@ use Git;
 
 print "Content-type: text/plain\n\n";
 
+# On quitte si le POST est absent
+use CGI qw();
+my $c = CGI->new;
+my $gitHubHook = $c->header('text/plain');
+if( !($c->request_method eq 'POST' && $c->param( 'payload' )) )
+{
+  my $filename = 'error.log';
+  open( my $fh, '>', $filename ) or die "Could not open file '$filename' $!";
+  print $fh "Erreur\n$now";
+  close $fh;
+  exit;
+}
+
 # Enregistre l’heure de la dernière synchronisation dans un fichier texte.
 my $now = strftime( '{"github_webhook_last_pull_time":"%Y-%m-%d %H:%M:%S"}', localtime );
 my $filename = 'github-webhook-log.json';
@@ -18,20 +31,3 @@ close $fh;
 my $pwd = `pwd`; chomp( $pwd );
 my $repo = Git->repository( Directory => $pwd );
 my $msg = $repo->command( "pull" );
-
-# Test si POST présent
-use CGI qw();
-my $c = CGI->new;
-my $gitHubHook = $c->header('text/plain');
-my $ans = '';
-if( !($c->request_method eq 'POST' && $c->param( 'payload' )) )
-{
-    $ans = "NO\n";
-} else {
-	$ans = "YES\n";
-}
-my $filename = 'github-ans.log';
-open( my $fh, '>', $filename ) or die "Could not open file '$filename' $!";
-print $fh $ans;
-print $fh $gitHubHook;
-close $fh;
